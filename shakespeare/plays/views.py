48c1b7553d0b4566c1b49a029ade4ae3b9178c1d
Zack Stout <zackstout@gmail.com>
2018-07-25 15:18:10 -0500

    removing quotation marks

--- shakespeare/plays/views.py
+++ shakespeare/plays/views.py
@@ -2,12 +2,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import PlayName, PlayText
-import requests, datetime, time
+import requests, datetime, time, re
 
 # Create your views here.
 def index(request, id):
     p = PlayName.objects.all()[:20]
     text = PlayText.objects.filter(play_id=id).order_by('act', 'scene', 'lineno')
+
+    for t in text:
+        t.text = re.sub('^"', '', t.text)
+        t.text = re.sub('"$', '', t.text)
+        # print(t.text)
 
     # return HttpResponse(text)
     context = {'text': text}
